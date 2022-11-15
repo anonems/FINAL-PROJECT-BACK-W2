f5e4ea25d2bb044b402ed213114ffc9fdc95713c
anonems <93661372+anonems@users.noreply.github.com>
2022-11-15 16:38:05 +0100

    add database and docker config

---
+++ src/views/homepage_template.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+        <meta charset="UTF-8">
+        <title>BLOG - DOCKER COURS 1-2</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/css/swiper.min.css'>
+        <link rel="stylesheet" href="assets/style/style.css">
+    </head>
+    <body>
+    <div class="blog-slider">
+        <div class="blog-slider__wrp swiper-wrapper">
+            <?php
+                if(!isset($_SESSION['connecte'])){ //not connected => login/signin
+                    include('./auth/login.php');
+                }elseif($_SESSION['connecte']){ //connected => home blog
+                    include('./blog/blog.php');
+                }
+            ?>
+        </div>
+        <div class="blog-slider__pagination"></div>
+    </div>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/js/swiper.min.js'></script><script  src="assets/script/script.js"></script>
+    </body>
+    <footer>
+    </footer>
+</html>
