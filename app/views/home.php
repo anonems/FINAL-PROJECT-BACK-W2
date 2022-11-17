695d88ebb722dfe5d4722249c60ddc571a85c8fb
Nassim <113092073+NasssDev@users.noreply.github.com>
2022-11-17 13:57:29 +0100

    pubDate on home.php

--- app/views/home.php
+++ app/views/home.php
@@ -6,7 +6,7 @@ foreach($articles as $article):
         <div class="blog-slider__item swiper-slide">
             <div class="blog-slider__img">
                 <img src="<?=$article->getIllustration()?>">
-            </div><!---->
+            </div>
             <div class="blog-slider__content">
                 <span class="blog-slider__code"><?=date('Y-m-d H:i:s',$article->getPubdate())?> by @<?=$article->getAuthor()?> <a id='login_bt' style='text-decoration:none;' href='./index.php'>HOME</a></span>
                 <div class="blog-slider__title"><?=$article->getTitle()?></div>
